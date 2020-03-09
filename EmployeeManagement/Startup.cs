6ec57c7d3fa91fcb55e72b3a7fdcbca4cffcf51a
ismail5g <ismail27.dec@gmail.com>
2020-03-09 22:14:24 +0600

    MVC setup

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -22,7 +22,7 @@ namespace EmployeeManagement
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +38,6 @@ namespace EmployeeManagement
             }
 
             app.UseStaticFiles();
-
             app.UseRouting();
 
             app.UseAuthorization();
